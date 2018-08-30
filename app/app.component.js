ba10e0917e7aa6340c81befb057dbdc1989e1f23
zietara.sadadamzian1 <zietara.dazzzmian1@gmail.com>
2018-08-30 15:58:13 +0200

    rozdzielenie poszczegolnych komponentow itp na inne pliki

---
+++ app/app.component.js
@@ -0,0 +1,16 @@
+(function(app){
+
+    var Component = ng.core.Component;
+
+    
+app.AppComponent = Component({
+    selector: 'my-app',
+    template: `<h1>Angular 2.0</h1>
+              <second></second>`
+    })
+.Class({
+    constructor: function AppComponent(){}
+});
+
+
+})(window.app || (window.app = {}));
---
+++ app/main.js
@@ -0,0 +1,6 @@
+(function(app){
+    var platformBrowserDynamic = ng.platformBrowserDynamic.platformBrowserDynamic;
+    var AppModule = app.AppModule;
+platformBrowserDynamic().bootstrapModule(AppModule);
+
+})(window.app || (window.app = {}));
