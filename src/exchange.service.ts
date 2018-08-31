28ece24cd9c3b2c3c6fe18147d471c2bbe0547fb
zietara.sadadamzian1 <zietara.dazzzmian1@gmail.com>
2018-08-31 16:09:39 +0200

    zmiany walut

---
+++ src/exchange.service.ts
@@ -0,0 +1,17 @@
+
+
+export class ExchangeService{
+
+    private exchangeRate = {
+        'USD/GBP': 0.7,
+        'USD/EUR':1.30,
+        'EUR/USD':1.30,
+        'GBP/EUR':1.24,
+        'GBP/USD':1.42
+    }
+
+    getExchangeRate(baseCurrency:string,targetCurrency:string){
+        return this.exchangeRate[baseCurrency + '/'+ targetCurrency]
+    }
+
+}
--- src/third.component.ts
+++ src/third.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import {ExchangeService} from './exchange.service';
 
 @Component({
     selector: 'converter',
+    providers:[ExchangeService],
     template: `<h2>Konwerter Walutowy</h2>
             Przelicz: <input type="number" [(ngModel)]="baseAmount"
-            [ngClass]="{error:isValid(baseAmount), warning: baseAmount<0}"> USD =
-                    <strong>{{targetAmount}}</strong> GBP  
+            [ngClass]="{error:isValid(baseAmount), warning: baseAmount<0}"> {{baseCurrency}} =
+                    <strong>{{targetAmount}}</strong> {{targetCurrency}}  
             `,
     styles:[` input[type=number] {
                 width: 10ex;
@@ -22,11 +24,19 @@ import { Component } from '@angular/core';
 })
 export class ThirdComponent {
 
-    exchangeRate = 0.70;
+    baseCurrency= "USD";
+    targetCurrency="GBP";
     baseAmount = 1;
 
+ 
+
+    constructor(private exchangeService: ExchangeService){}
+
+    
     get targetAmount(){
-       return this.baseAmount * this.exchangeRate
+        const exchangeRate = this.exchangeService.
+            getExchangeRate(this.baseCurrency,this.targetCurrency);
+       return this.baseAmount * exchangeRate
     }
 
     isValid(value){
