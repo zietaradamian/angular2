f8c28e5efa4baa4324f34a2308b0ce296edce4b5
zietara.sadadamzian1 <zietara.dazzzmian1@gmail.com>
2018-09-06 14:14:30 +0200

    dodanie pipes

--- src/third.component.ts
+++ src/third.component.ts
@@ -8,7 +8,7 @@ import {ExchangeService} from './exchange.service';
             Przelicz: <input type="number" [(ngModel)]="baseAmount"
             [ngClass]="{error:isValid(baseAmount), warning: baseAmount<0}">
             <currency-select [(selected)]="baseCurrency"></currency-select>
-            = <strong>{{targetAmount}}</strong> 
+            = <strong>{{targetAmount | number:'1.2-2'}}</strong> 
             <currency-select [(selected)]="targetCurrency"></currency-select>
             
             <p *ngIf="isValid(baseAmount)">Proszę wprowadzić wartość do przeliczenia.</p>  
